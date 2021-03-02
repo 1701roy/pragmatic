29113a68e486a7954b92539403590a6a43762683
royp2 <1701roy@gmail.com>
2021-03-01 23:51:40 -0800

    Linked List ADT

--- llist.cpp
+++ llist.cpp
@@ -32,6 +32,9 @@ class LinkedList
     LinkedList();
     void Display();
     void RDisplay(Node *p);
+    LinkedList(int *p, int n);
+    int getLength();
+    int RgetLength(Node *p);
 };
 
 LinkedList::LinkedList()
@@ -51,6 +54,7 @@ void LinkedList::Display()
         p = p->next; 
     }
     std::cout<<std::endl;
+    return;
 }
 //Time Complexity: O(n), where n is the length of the linked list
 //Space Complexity: O(n), n+1 activation records will be created in the stack
@@ -61,13 +65,66 @@ void LinkedList::RDisplay(Node *p)
         std::cout<<p->data<<" ";
         RDisplay(p->next);
     }
+    return;
+}
+
+//Time Complexity: O(n), where n is the size of the array
+LinkedList::LinkedList(int *p, int n)
+{
+    Node *q, *last;
+    first = new Node;
+    first->data = p[0];
+    first->next = nullptr;
+    last = first;
+
+    for(int i=1;i<n;i++)
+    {
+        q = new Node;
+        q->data = p[i];
+        q->next = nullptr;
+        last->next = q;
+        last = q;
+    }
+    return;
+}
+
+//Time Complexity: O(n), where n is the number of nodes/length in the LL
+int LinkedList::getLength()
+{
+    int length = 0;
+    Node *p = first;
+    while(p!=nullptr)
+    {
+        length += 1;
+        p = p->next;
+    }
+
+    return length;
+}
+
+//Time Complexity: O(n), where n is the length of the list
+//Space Complexity: O(n), n+1 activation records
+int LinkedList::RgetLength(Node *p)
+{
+    if(p==nullptr)
+    {
+        return 0; 
+    }
+    else
+    {
+        return RgetLength(p->next) + 1;
+    }
 }
 
 int main()
 {
-    LinkedList a;
-    a.RDisplay(a.first);
+    int *a;
+    int arr[10] = {1,2,3,4};
+    a = arr;
+    LinkedList lst(a,10);
+    lst.Display();
     std::cout<<std::endl;
+    std::cout<<lst.getLength()<<std::endl;
     return 0;
 }
 
