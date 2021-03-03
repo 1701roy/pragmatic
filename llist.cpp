34d6002fbe51d8f48b013a9795454ed2ca98bf85
royp2 <1701roy@gmail.com>
2021-03-02 23:52:11 -0800

    Linked List ADT

--- llist.cpp
+++ llist.cpp
@@ -17,6 +17,7 @@
  */
 
 #include<iostream>
+#include<limits>
 
 class Node
 {
@@ -35,6 +36,11 @@ class LinkedList
     LinkedList(int *p, int n);
     int getLength();
     int RgetLength(Node *p);
+    int max();
+    int Rmax(Node *p);
+    Node* search(const int val);
+    Node* Rsearch(Node *p, const int val);
+    void Insert(const int pos, const int val);
 };
 
 LinkedList::LinkedList()
@@ -116,6 +122,88 @@ int LinkedList::RgetLength(Node *p)
     }
 }
 
+//Time Complexity: O(n), where n is the number of elements in the list
+int LinkedList::max()
+{
+    int max = std::numeric_limits<int>::min(); 
+    Node *p = first;
+    while(p!=nullptr)
+    {
+        max = p->data > max ? p->data : max;
+        p = p->next;
+    }
+    return max;
+}
+
+//Time Complexity: O(n), where n is the numner of elements in the list
+//Space Complexity: O(n), as there will be n+1 activation records
+int LinkedList::Rmax(Node *p)
+{
+    if(p==nullptr)
+    {
+        return std::numeric_limits<int>::min();
+    }
+    else
+    {
+        int x = 0;
+        x = Rmax(p->next);
+        return x > p->data ? x : p->data;
+    }
+}
+
+//Time Complexity: Best O(1), if the head element itself is the searched value
+//                 Worst O(n), if the value is not present or the last element
+Node* LinkedList::search(const int val)
+{
+    Node *p = first;
+    while(p!=nullptr)
+    {
+        if(p->data == val)
+        {
+            return p;
+        }
+        p = p->next;
+    }
+    return nullptr;
+}
+
+//Time Complexity: Best O(1), if the head element itself is the searched value
+//                 Worst O(n), if the value is not present or the last element
+//Space Complexity: Worst O(n), if the value is not preesnt or the last element
+Node* LinkedList::Rsearch(Node *p,const int val)
+{
+    if(p==nullptr)
+        return nullptr;
+
+    return p->data==val ? p : Rsearch(p->next, val);
+}
+
+//Time Complexity: Best O(1), adding at 0th position
+//                 Worst O(n), adding at the last element position
+void LinkedList::Insert(const int pos, const int val)
+{
+    Node *p = first;
+    for(int i=0;i<pos-1 && p;i++)
+    {
+        p = p->next;
+    }
+    if(p)
+    {
+        Node *t = new Node;
+        t->data = val;
+        if(pos == 0)
+        {
+            t->next = p;
+            first = t;
+        }
+        else
+        {
+            t->next = p->next;
+            p->next = t;
+        }
+    }
+}
+
 int main()
 {
     int *a;
@@ -123,8 +211,8 @@ int main()
     a = arr;
     LinkedList lst(a,10);
     lst.Display();
-    std::cout<<std::endl;
-    std::cout<<lst.getLength()<<std::endl;
+    lst.Insert(10,15);
+    lst.Display();
     return 0;
 }
 
